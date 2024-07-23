230506d7308972f15469a94bba6de41ee4724cbe
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-07-23 12:07:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const quoteBtn = document.getElementById('newQuote');
-const quotes= [
- { text: "The best way to predict the future is to create it.", category: "Motivational" },
-  { text: "A laugh is the shortest distance between two people.", category: "Funny" },
-]
-function randomQuote(){
-    
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQ = quotes[randomIndex];
-    quoteBtn.addEventListener('click', 
-        document.getElementById('quoteDisplay').innerHTML( randomQ)
-    );
+let quotes= [
+    {text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" },
+    {text: "Life is what happens when you're busy making other plans.", category: "Life"},
+];
+function showRandomQuote()  {
+    const randomIndex= Math.floor(Math.random()* quotes.length);
+    const quote = quotes[randomIndex];
+    document.getElementById('quoteDisplay').innerHTML= `"${quote.text}"-${quote.category}`;
     return;
 }
-randomQuote();
+function addQuote(){
+   const newQuoteText= document.getElementById('newQuoteText').value;
+   const newQuoteCategory=document.getElementById('newQuoteCategory').value;
+   if(newQuoteText&& newQuoteCategory){
+    quotes.push({text:newQuoteText,category:newQuoteCategory})
+    document.getElementById('newQuoteText').value='';
+    document.getElementById('newQuoteCategory').value='';
+    alert("quote added succesfully");
+   }else{
+    alert("please add a quote ");
+   }
+}
+document.getElementById('newQuote').addEventListener('click',showRandomQuote);
